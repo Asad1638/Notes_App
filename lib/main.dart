75c094322fc19b012828ba67a3f501886bf058b5
Asad1638 <asadullah.soft@gmail.com>
2024-03-14 12:41:17 +0500

    tile changed

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Home Page',
+      title: 'My Home',
       theme: ThemeData(
      brightness: Brightness.dark,
         useMaterial3: true,
