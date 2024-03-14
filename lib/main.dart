36591061d32921f37c513eb3303c0e5570f3ac5e
Asad1638 <asadullah.soft@gmail.com>
2024-03-14 12:48:44 +0500

    n

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Notes_App',
+      title: 'Notes_App_hive',
       theme: ThemeData(
      brightness: Brightness.dark,
         useMaterial3: true,
