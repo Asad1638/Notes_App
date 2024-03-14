107e19e312cbf1e4c891556cffc485773cd4c4c1
Asad1638 <163280839+Asad1638@users.noreply.github.com>
2024-03-14 12:42:39 +0500

    Update main.dart
    title updated

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Home',
+      title: 'Notes_App',
       theme: ThemeData(
      brightness: Brightness.dark,
         useMaterial3: true,
